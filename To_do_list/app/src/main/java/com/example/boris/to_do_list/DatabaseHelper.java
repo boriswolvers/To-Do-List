a0f3bfc596206b9cd0eae0019e9c15b8bea42aca
Boris Wolvers <bwolvers@live.nl>
2016-11-23 01:37:56 +0100

    app

---
+++ To_do_list/app/src/main/java/com/example/boris/to_do_list/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.example.boris.to_do_list;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // Name of the table
+    public static final String TABLE_NAME = "TO_DO_ITEMS";
+
+    // Table columns
+    public static final String _ID = "_id";
+    public static final String DESC_OF_TODO_ITEM = "description_of_todo_item";
+
+    // Database Information
+    static final String DB_NAME = "TODOLIST.DB";
+
+    // Database version
+    static final int DB_VERSION = 1;
+
+    // Creating table query
+    private static final String CREATE_TABLE = "create table " + TABLE_NAME + "(" + _ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DESC_OF_TODO_ITEM + " TEXT NOT NULL);";
+
+    // The constructor
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
